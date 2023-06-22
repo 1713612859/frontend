08989301e250468bd16227a721f3391ea909b377
xiaoliu <1713612859@qq.com>
2023-06-22 23:13:27 +0800

    ts demo

---
+++ src/ts/test/1.ts
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param age number
+ * @returns adult | no adult
+ */
+function sayHello(age: number) {
+    if (age >= 18) return '"adult"'
+    return 'no adult'
+}
+
+console.log(sayHello(555));
+
+
+function catName(params: Array<any>) {
+    params.forEach(element => {
+        console.log(element);
+    });
+}
+
+catName([1, 5, 6, 9, 8, 3, 6])
