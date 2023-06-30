d9d3beb363c27689727ff73a44f5d5c5e45517d5
xiaoliu <1713612859@qq.com>
2023-06-30 10:58:48 +0800

    今天写写测试

--- src/ts/test/2.ts
+++ src/ts/test/2.ts
@@ -1,1 +1,14 @@
+/**
+ * 测试参数限定
+ * @param name "abc" | "abc2" | "abc3"
+ * @returns  name
+ */
+function test(name: "abc" | "abc2" | "abc3") {
+    return name
+}
+
+
+console.log(
+    test("abc2"));
+
 
